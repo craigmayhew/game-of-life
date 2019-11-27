e6a126cc1c95bbb382054f7d97b732e11331b4cc
Craig Mayhew <craig@mayhew.io>
2019-11-27 13:27:25 +0000

    Assumed we are now reading display.ron

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,14 @@
 extern crate amethyst;
-use amethyst::prelude::*;
+use amethyst::{
+    prelude::*,
+    /*renderer::{
+        plugins::{RenderFlat2D, RenderToWindow},
+        types::DefaultBackend,
+        RenderingBundle,
+    },*/
+    //needed for application_root_dir() etc
+    utils::application_root_dir,
+};
 
 struct GameplayState {
     /// The `State`-local data. Usually you will not have anything.
@@ -14,6 +23,11 @@ impl State<GameData<'static, 'static>, ()> for GameplayState {
     }
 }
 
-fn main() {
-    println!("Hello, world!");
+fn main() -> amethyst::Result<()> {
+    //amethyst::start_logger(Default::default());
+    
+    let app_root = application_root_dir()?;
+    let display_config_path = app_root.join("config").join("display.ron");
+
+    Ok(())
 }
