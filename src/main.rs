36c9c77d51b399b5eced00df6997d03d023b70ee
Craig Mayhew <craig@mayhew.io>
2019-11-27 13:20:15 +0000

    Added a simple state

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+extern crate amethyst;
+use amethyst::prelude::*;
+
+struct GameplayState {
+    /// The `State`-local data. Usually you will not have anything.
+    /// In this case, we have the number of players here.
+    player_count: u8,
+}
+
+//GameData is the internal shared data between states
+impl State<GameData<'static, 'static>, ()> for GameplayState {
+    fn on_start(&mut self, _data: StateData<'_, GameData<'_, '_>>) {
+        println!("Number of players: {}", self.player_count);
+    }
+}
+
 fn main() {
     println!("Hello, world!");
 }
