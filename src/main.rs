a65621b7040a632870ff658521fb723760f855b9
Craig Mayhew <craig@mayhew.io>
2019-11-27 13:34:59 +0000

    We now have a window that appears with a title when we "cargo run"!

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
 extern crate amethyst;
 use amethyst::{
     prelude::*,
-    /*renderer::{
+    //renderer is used to display a window
+    renderer::{
         plugins::{RenderFlat2D, RenderToWindow},
         types::DefaultBackend,
         RenderingBundle,
-    },*/
+    },
     //needed for application_root_dir() etc
     utils::application_root_dir,
 };
@@ -27,7 +28,17 @@ fn main() -> amethyst::Result<()> {
     let app_root = application_root_dir()?;
     let display_config_path = app_root.join("config").join("display.ron");
 
-    let game_data = GameDataBuilder::default();
+    let game_data = GameDataBuilder::default()
+    .with_bundle(
+        RenderingBundle::<DefaultBackend>::new()
+            // The RenderToWindow plugin provides all the scaffolding for opening a window and drawing on it
+            .with_plugin(
+                RenderToWindow::from_config_path(display_config_path)
+                    .with_clear([0.0, 0.0, 0.0, 1.0]),
+            )
+            // RenderFlat2D plugin is used to render entities with a `SpriteRender` component.
+            .with_plugin(RenderFlat2D::default()),
+    )?;
 
     let assets_dir = app_root.join("assets");
     let mut world = World::new();
