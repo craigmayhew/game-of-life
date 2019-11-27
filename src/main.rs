b7c64017f307da1928cd26b3176fbacc3bef3ea5
Craig Mayhew <craig@mayhew.io>
2019-11-27 13:39:45 +0000

    Added fn initialise_camera()

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 extern crate amethyst;
 use amethyst::{
+    core::transform::Transform,
     prelude::*,
     //renderer is used to display a window
     renderer::{
+        Camera,
         plugins::{RenderFlat2D, RenderToWindow},
         types::DefaultBackend,
         RenderingBundle,
@@ -11,8 +13,23 @@ use amethyst::{
     utils::application_root_dir,
 };
 
+pub const ARENA_HEIGHT: f32 = 100.0;
+pub const ARENA_WIDTH: f32 = 100.0;
+
 struct GameplayState {
     lifeforms: u8,
+}
+
+fn initialise_camera(world: &mut World) {
+    // Setup camera in a way that our screen covers whole arena and (0, 0) is in the bottom left. 
+    let mut transform = Transform::default();
+    transform.set_translation_xyz(ARENA_WIDTH * 0.5, ARENA_HEIGHT * 0.5, 1.0);
+
+    world
+        .create_entity()
+        .with(Camera::standard_2d(ARENA_WIDTH, ARENA_HEIGHT))
+        .with(transform)
+        .build();
 }
 
 //GameData is the internal shared data between states
