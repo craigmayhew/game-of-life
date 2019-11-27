f4f005c3e2f9e9993e5e5ef3d67d986a14c6ba61
Craig Mayhew <craig@mayhew.io>
2019-11-27 13:31:49 +0000

    Amethyst logger is now running!

--- src/main.rs
+++ src/main.rs
@@ -22,7 +22,7 @@ impl SimpleState for GameplayState {
 }
 
 fn main() -> amethyst::Result<()> {
-    //amethyst::start_logger(Default::default());
+    amethyst::start_logger(Default::default());
     
     let app_root = application_root_dir()?;
     let display_config_path = app_root.join("config").join("display.ron");
