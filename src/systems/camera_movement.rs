d208317512a3baedaa82d4f52b64567297ad0566
Craig Mayhew <craig@mayhew.io>
2019-11-27 15:58:59 +0000

    Look left and look right implemented and working with camera!

--- src/systems/camera_movement.rs
+++ src/systems/camera_movement.rs
@@ -26,20 +26,29 @@ impl<'s> System<'s> for CameraMovementSystem {
             println!("camera: {}", name.name);
             
             if name.name == "Main camera" {
+                // move up / down
                 if input_handler.action_is_down("CameraMoveUp").unwrap_or(false) {
                     transform.move_up(move_factor);
                 } else if input_handler.action_is_down("CameraMoveDown").unwrap_or(false) {
                     transform.move_down(move_factor);
                 }
+                // move left /right
                 if input_handler.action_is_down("CameraMoveLeft").unwrap_or(false) {
                     transform.move_left(move_factor);
                 } else if input_handler.action_is_down("CameraMoveRight").unwrap_or(false) {
                     transform.move_right(move_factor);
                 }
+                // move forward / backward
                 if input_handler.action_is_down("CameraMoveForward").unwrap_or(false) {
                     transform.move_forward(move_factor);
                 } else if input_handler.action_is_down("CameraMoveBackward").unwrap_or(false) {
                     transform.move_backward(move_factor);
+                }
+                // look left / right
+                if input_handler.action_is_down("CameraLookLeft").unwrap_or(false) {
+                    transform.append_rotation_y_axis(0.02);
+                } else if input_handler.action_is_down("CameraLookRight").unwrap_or(false) {
+                    transform.append_rotation_y_axis(-0.02);
                 }
             }
         }
