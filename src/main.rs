3c7a678bb4e2ac8c60b206470e78d98df2fb4cdc
Craig Mayhew <craig@mayhew.io>
2020-04-13 12:09:14 +0100

    Moved hard coded color vector into variable

--- src/main.rs
+++ src/main.rs
@@ -209,13 +209,15 @@ fn main() -> amethyst::Result<()> {
     let input_bundle = InputBundle::<StringBindings>::new()
     .with_bindings_from_file(binding_path)?;
 
+    let black = [0.0, 0.0, 0.0, 1.0];
+
     let game_data = GameDataBuilder::default()
     .with_bundle(
         RenderingBundle::<DefaultBackend>::new()
             // The RenderToWindow plugin provides all the scaffolding for opening a window and drawing on it
             .with_plugin(
                 RenderToWindow::from_config_path(display_config_path)?
-                    .with_clear([0.0, 0.0, 0.0, 1.0]),
+                    .with_clear(black),
             )
             // RenderFlat2D plugin is used to render entities with a `SpriteRender` component.
             .with_plugin(RenderFlat2D::default())
