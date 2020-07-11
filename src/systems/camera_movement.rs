32c385259b65a7c28b2ad42b7bace414c329aa88
Craig Mayhew <craig@mayhew.io>
2020-07-11 15:23:51 +0100

    Removed camera name check code. As we only have a single camera!

--- src/systems/camera_movement.rs
+++ src/systems/camera_movement.rs
@@ -1,5 +1,5 @@
 use amethyst::{
-    core::{Named, Time, Transform},
+    core::{Time, Transform},
     ecs::*,
     input::{InputHandler, StringBindings},
     renderer::camera::Camera,
@@ -11,42 +11,39 @@ pub struct CameraMovementSystem {}
 impl<'s> System<'s> for CameraMovementSystem {
     type SystemData = (
         ReadStorage<'s, Camera>,
-        ReadStorage<'s, Named>,
         WriteStorage<'s, Transform>,
         Read<'s, InputHandler<StringBindings>>,
         Read<'s, Time>,
     );
 
-    fn run(&mut self, (cameras, names, mut transforms, input_handler, time): Self::SystemData) {
+    fn run(&mut self, (cameras, mut transforms, input_handler, time): Self::SystemData) {
         //delta_real_seconds() allows us to move at a consistent speed, irrespective of the frame rate
         let delta_time = time.delta_real_seconds();
         let move_factor = 1000.0 * delta_time;
-        for (_, name, transform) in (&cameras, &names, &mut transforms).join() {
-            if name.name == "Main camera" {
-                // move up / down
-                if input_handler.action_is_down("CameraMoveUp").unwrap_or(false) {
-                    transform.move_up(move_factor);
-                } else if input_handler.action_is_down("CameraMoveDown").unwrap_or(false) {
-                    transform.move_down(move_factor);
-                }
-                // move left /right
-                if input_handler.action_is_down("CameraMoveLeft").unwrap_or(false) {
-                    transform.move_left(move_factor);
-                } else if input_handler.action_is_down("CameraMoveRight").unwrap_or(false) {
-                    transform.move_right(move_factor);
-                }
-                // move forward / backward
-                if input_handler.action_is_down("CameraMoveForward").unwrap_or(false) {
-                    transform.move_forward(move_factor);
-                } else if input_handler.action_is_down("CameraMoveBackward").unwrap_or(false) {
-                    transform.move_backward(move_factor);
-                }
-                // look left / right
-                if input_handler.action_is_down("CameraLookLeft").unwrap_or(false) {
-                    transform.append_rotation_y_axis(0.02);
-                } else if input_handler.action_is_down("CameraLookRight").unwrap_or(false) {
-                    transform.append_rotation_y_axis(-0.02);
-                }
+        for (_, transform) in (&cameras, &mut transforms).join() {
+            // move up / down
+            if input_handler.action_is_down("CameraMoveUp").unwrap_or(false) {
+                transform.move_up(move_factor);
+            } else if input_handler.action_is_down("CameraMoveDown").unwrap_or(false) {
+                transform.move_down(move_factor);
+            }
+            // move left /right
+            if input_handler.action_is_down("CameraMoveLeft").unwrap_or(false) {
+                transform.move_left(move_factor);
+            } else if input_handler.action_is_down("CameraMoveRight").unwrap_or(false) {
+                transform.move_right(move_factor);
+            }
+            // move forward / backward
+            if input_handler.action_is_down("CameraMoveForward").unwrap_or(false) {
+                transform.move_forward(move_factor);
+            } else if input_handler.action_is_down("CameraMoveBackward").unwrap_or(false) {
+                transform.move_backward(move_factor);
+            }
+            // look left / right
+            if input_handler.action_is_down("CameraLookLeft").unwrap_or(false) {
+                transform.append_rotation_y_axis(0.02);
+            } else if input_handler.action_is_down("CameraLookRight").unwrap_or(false) {
+                transform.append_rotation_y_axis(-0.02);
             }
         }
     }
