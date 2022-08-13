90608958c96f4fe04f41f2d78eb28834aac4f2e5
Craig Mayhew <craig@mayhew.io>
2022-08-13 15:19:46 +0100

    Generation counter now displayed too. Counters are away from screen edge.

--- src/systems/hud.rs
+++ src/systems/hud.rs
@@ -7,7 +7,7 @@ use crate::{
 };
 
 pub struct HudData {
-    life_counter: Entity,
+    counters: Entity,
 }
 
 // A unit struct to help identify the life counter UI component, since there may be many Text components
@@ -15,33 +15,33 @@ pub struct HudData {
 pub struct LifeCounterText;
 
 pub fn enter(mut commands: Commands, asset_server: Res<AssetServer>) {
-    let button_text_style = TextStyle {
+    let text_style = TextStyle {
         font: asset_server.load("font/square.ttf"),
         font_size: 20.0,
         color: Color::rgb(0.9, 0.9, 0.9),
     };
-    let life_counter = commands
+    let counters = commands
         .spawn_bundle(
             TextBundle::from_section(
                 "Life detected: 0000000",
-                button_text_style,
+                text_style,
             )
-            .with_text_alignment(TextAlignment::TOP_CENTER)
+            .with_text_alignment(TextAlignment::TOP_LEFT)
         )
         .insert(LifeCounterText)
         .id();
-    commands.insert_resource(HudData { life_counter });
+    commands.insert_resource(HudData { counters });
 }
 
 pub fn run(
     session: ResMut<SessionResource>,
-    mut query: Query<&mut Text, With<LifeCounterText>>
+    mut life_counter: Query<&mut Text, With<LifeCounterText>>,
 ) {
-    for mut text in &mut query {
-        text.sections[0].value = format!("Life detected: {:07}",&session.counter);
+    for mut text in &mut life_counter {
+        text.sections[0].value = format!("    Generation:       {:07}\n    Life detected: {:07}\n ",&session.generation,&session.counter);
     }
 }
 
 pub fn cleanup(mut commands: Commands, hud_data: Res<HudData>) {
-    commands.entity(hud_data.life_counter).despawn_recursive();
+    commands.entity(hud_data.counters).despawn_recursive();
 }
