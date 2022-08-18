3c104225d1fc1080952b77861ad55d3e7e4d582e
Craig Mayhew <craig@mayhew.io>
2022-08-18 22:39:23 +0100

    loading a file will now catch and print errors related to corrupt save files

--- src/systems/saves.rs
+++ src/systems/saves.rs
@@ -33,42 +33,49 @@ pub fn load (
     
     let contents = read_to_string("saves/test.ron").expect("Failed to load save file");
 
-    let data: SaveResource = ron::from_str(&contents).unwrap();
-    
-    // unspawn every single life entity
-    for ent in life_entities.iter_mut() {
-        commands.entity(ent.to_owned()).despawn();
-    }
+    let result = ron::from_str::<SaveResource>(&contents);
 
-    // resize the universe to match the load file
-    session.life = vec![vec![vec![vec![crate::systems::life::LifeDataContainer::Dead(true); data.universe_size]; data.universe_size]; data.universe_size]; 6];
+    match result {
+        Ok(data) => {
+            // unspawn every single life entity
+            for ent in life_entities.iter_mut() {
+                commands.entity(ent.to_owned()).despawn();
+            }
 
-    // so we can spawn new ones form the save file
-    for (n, vec1) in data.life.iter().enumerate() {
-        for (x, vec2) in vec1.iter().enumerate() {
-            for (y, vec3) in vec2.iter().enumerate() {
-                for (z, alive_or_not) in vec3.iter().enumerate() {
-                    if alive_or_not == &1 {
-                        let transform_new_life: bevy::prelude::Transform = crate::systems::life::create_life_xyz(n, x, y, z);
-                    
-                        // make the life form exist!
-                        session.life[n][x][y][z] = LifeDataContainer::Alive(commands.spawn_bundle(PbrBundle {
-                            mesh: session.life_form_meshes[n%2].clone(),
-                            material: session.life_form_materials[n].clone(),
-                            transform: transform_new_life,
-                            ..Default::default()
-                        }).insert(Life).id());
-                    } else {
-                        session.life[n][x][y][z] = LifeDataContainer::Dead(true);
+            // resize the universe to match the load file
+            session.life = vec![vec![vec![vec![crate::systems::life::LifeDataContainer::Dead(true); data.universe_size]; data.universe_size]; data.universe_size]; 6];
+
+            // so we can spawn new ones form the save file
+            for (n, vec1) in data.life.iter().enumerate() {
+                for (x, vec2) in vec1.iter().enumerate() {
+                    for (y, vec3) in vec2.iter().enumerate() {
+                        for (z, alive_or_not) in vec3.iter().enumerate() {
+                            if alive_or_not == &1 {
+                                let transform_new_life: bevy::prelude::Transform = crate::systems::life::create_life_xyz(n, x, y, z);
+                            
+                                // make the life form exist!
+                                session.life[n][x][y][z] = LifeDataContainer::Alive(commands.spawn_bundle(PbrBundle {
+                                    mesh: session.life_form_meshes[n%2].clone(),
+                                    material: session.life_form_materials[n].clone(),
+                                    transform: transform_new_life,
+                                    ..Default::default()
+                                }).insert(Life).id());
+                            } else {
+                                session.life[n][x][y][z] = LifeDataContainer::Dead(true);
+                            }
+                        }
                     }
                 }
             }
-        }
-    }
 
-    session.counter = data.counter;
-    session.generation = data.generation;
-    session.universe_size = data.universe_size;
+            session.counter = data.counter;
+            session.generation = data.generation;
+            session.universe_size = data.universe_size;
+        },
+        Err(e) => {
+            println!("Saves System, Error loading save file: {}", e);
+        },
+    }
 
     // in bevy 0.8 overwrite_set() is needed instead of set() when system is called via on_enter()
     let res = state.overwrite_set(AppState::Paused);
