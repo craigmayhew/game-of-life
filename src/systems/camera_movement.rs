0f3e93649188a1a7581279562c54ca744961863d
Craig Mayhew <craig@mayhew.io>
2024-02-28 11:52:22 +0000

    rust fmt

--- src/systems/camera_movement.rs
+++ src/systems/camera_movement.rs
@@ -10,75 +10,74 @@ use bevy::{
 use crate::{systems::life::LIFE_FORM_SIZE, DEFAULT_UNIVERSE_SIZE};
 
 pub fn setup(mut commands: Commands) {
-    commands.spawn((
-        Camera3dBundle {
-            camera: Camera {
-                hdr: true, // HDR is required for bloom
-                ..default()
-            },
-            tonemapping: Tonemapping::TonyMcMapface, // Using a tonemapper that desaturates to white is recommended
-            projection: PerspectiveProjection {
-                near: 0.1,
-                far: 100_000_000.0,
-                aspect_ratio: 16.0 / 9.0,
-                fov: std::f32::consts::FRAC_PI_3,
-            }
-            .into(),
-            exposure: Exposure::from_physical_camera(PhysicalCameraParameters {
-                aperture_f_stops: 1.0,
-                shutter_speed_s: 1.0 / 100.0,
-                sensitivity_iso: 100.0,
-            }),
-            transform: Transform::from_xyz(
-                (DEFAULT_UNIVERSE_SIZE << 1) as f32 * -LIFE_FORM_SIZE,
-                (DEFAULT_UNIVERSE_SIZE >> 1) as f32 * LIFE_FORM_SIZE,
-                (DEFAULT_UNIVERSE_SIZE << 1) as f32 * LIFE_FORM_SIZE,
-            )
-            .looking_at(
-                Vec3::new(
-                    5_000_000.0,
+    commands
+        .spawn((
+            Camera3dBundle {
+                camera: Camera {
+                    hdr: true, // HDR is required for bloom
+                    ..default()
+                },
+                tonemapping: Tonemapping::TonyMcMapface, // Using a tonemapper that desaturates to white is recommended
+                projection: PerspectiveProjection {
+                    near: 0.1,
+                    far: 100_000_000.0,
+                    aspect_ratio: 16.0 / 9.0,
+                    fov: std::f32::consts::FRAC_PI_3,
+                }
+                .into(),
+                exposure: Exposure::from_physical_camera(PhysicalCameraParameters {
+                    aperture_f_stops: 1.0,
+                    shutter_speed_s: 1.0 / 100.0,
+                    sensitivity_iso: 100.0,
+                }),
+                transform: Transform::from_xyz(
+                    (DEFAULT_UNIVERSE_SIZE << 1) as f32 * -LIFE_FORM_SIZE,
                     (DEFAULT_UNIVERSE_SIZE >> 1) as f32 * LIFE_FORM_SIZE,
-                    0.0,
+                    (DEFAULT_UNIVERSE_SIZE << 1) as f32 * LIFE_FORM_SIZE,
+                )
+                .looking_at(
+                    Vec3::new(
+                        5_000_000.0,
+                        (DEFAULT_UNIVERSE_SIZE >> 1) as f32 * LIFE_FORM_SIZE,
+                        0.0,
+                    ),
+                    Vec3::Y,
                 ),
-                Vec3::Y,
-            ),
-            ..default()
-        },
-        BloomSettings::default(), // Enable bloom for the camera
-    ))
-    // add the ships lights that are fixed to the camera
-    .with_children(|builder| {
-        builder
-        .spawn(SpotLightBundle  {
-            transform: Transform::from_xyz(50.0, 0.0, 0.0),
-            spot_light: SpotLight {
-                color: Color::WHITE,
-                inner_angle: 0.1,
-                intensity: 1_000_000_000.0, // lumens
-                outer_angle: 0.5,
-                radius: 1.0,
-                range: 10_000.0,
-                shadows_enabled: true,
                 ..default()
             },
-            ..default()
-        });
-        builder
-        .spawn(SpotLightBundle  {
-            transform: Transform::from_xyz(-50.0, 0.0, 0.0),
-            spot_light: SpotLight {
-                color: Color::WHITE,
-                inner_angle: 0.1,
-                intensity: 1_000_000_000.0, // lumens
-                outer_angle: 0.5,
-                radius: 1.0,
-                range: 10_000.0,
-                shadows_enabled: true,
+            BloomSettings::default(), // Enable bloom for the camera
+        ))
+        // add the ships lights that are fixed to the camera
+        .with_children(|builder| {
+            builder.spawn(SpotLightBundle {
+                transform: Transform::from_xyz(50.0, 0.0, 0.0),
+                spot_light: SpotLight {
+                    color: Color::WHITE,
+                    inner_angle: 0.1,
+                    intensity: 1_000_000_000.0, // lumens
+                    outer_angle: 0.5,
+                    radius: 1.0,
+                    range: 10_000.0,
+                    shadows_enabled: true,
+                    ..default()
+                },
                 ..default()
-            },
-            ..default()
+            });
+            builder.spawn(SpotLightBundle {
+                transform: Transform::from_xyz(-50.0, 0.0, 0.0),
+                spot_light: SpotLight {
+                    color: Color::WHITE,
+                    inner_angle: 0.1,
+                    intensity: 1_000_000_000.0, // lumens
+                    outer_angle: 0.5,
+                    radius: 1.0,
+                    range: 10_000.0,
+                    shadows_enabled: true,
+                    ..default()
+                },
+                ..default()
+            });
         });
-    });
 }
 
 const ROTATE_SPEED: f32 = std::f32::consts::FRAC_1_PI / 5.0;
