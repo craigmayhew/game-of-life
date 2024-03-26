f6496f179b3d4d381d5e194c6344670506374aad
Craig Mayhew <craig@mayhew.io>
2024-03-26 14:49:53 +0000

    music now loops

--- src/systems/sound.rs
+++ src/systems/sound.rs
@@ -1,14 +1,29 @@
 use bevy::prelude::*;
+use bevy::audio::{PlaybackMode, Volume};
 
 use crate::SOUND_BG_LOOP;
+
+#[derive(Component)]
+pub struct BackgroundMusic;
+
 
 pub fn setup(mut audio_assets: ResMut<Assets<AudioSource>>, mut commands: Commands) {
     let audio_source_handle = audio_assets.add(AudioSource {
         bytes: SOUND_BG_LOOP.into(),
     });
 
-    commands.spawn(AudioBundle {
-        source: audio_source_handle,
-        ..default()
-    });
+    commands.spawn((
+        AudioBundle {
+            source: audio_source_handle,
+            settings: PlaybackSettings {
+                mode: PlaybackMode::Loop,
+                speed: 1.0,
+                paused: false,
+                spatial: false,
+                spatial_scale: None,
+                volume: Volume::new(1.0),
+            }
+        },
+        BackgroundMusic
+    ));
 }
