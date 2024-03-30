9a3cae9c306970f2fdc5e52aa2d0c18c51427333
Craig Mayhew <craig@mayhew.io>
2024-03-30 21:22:46 +0000

    Credits page now legible, though in single center col format

--- src/systems/menu_credits.rs
+++ src/systems/menu_credits.rs
@@ -4,8 +4,7 @@ use bevy::{
 
 #[derive(Resource)]
 pub struct MenuData {
-    credits_left: Entity,
-    credits_right: Entity,
+    credits: Entity,
 }
 // A unit struct to help identify CREDIT text UI component
 #[derive(Component)]
@@ -19,24 +18,12 @@ pub fn enter(mut commands: Commands, mut fonts: ResMut<Assets<Font>>) {
         font_size: 40.0,
         color: Color::rgb(0.9, 0.9, 0.9),
     };
-    let credits_left_str = r#"
-    
-    Concept:
-    Rust Engineer:
-    Soundtrack & SFX:       
-    "#;
+    let credits_left_str = "Concept & Rust Dev\nCraig Mayhew\n\nSoundtrack & SFX\nAnthony Howell";
 
-    let credits_right_str = r#"
-    CREDITS
-    Craig Mayhew
-    Craig Mayhew
-    Anthony Howell      
-    "#;
-
-    let credits_left = commands
+    let credits = commands
         .spawn(
             TextBundle::from_section(credits_left_str, text_style.clone())
-                .with_text_justify(JustifyText::Right)
+                .with_text_justify(JustifyText::Center)
                 .with_style(Style {
                     align_self: AlignSelf::Center,
                     margin: UiRect {
@@ -47,29 +34,6 @@ pub fn enter(mut commands: Commands, mut fonts: ResMut<Assets<Font>>) {
                     },
                     padding: UiRect {
                         left: Val::Auto,
-                        right: Val::Px(400.0),
-                        top: Val::Auto,
-                        bottom: Val::Auto,
-                    },
-                    ..default()
-                }),
-        )
-        .insert(CreditsText)
-        .id();
-    let credits_right = commands
-        .spawn(
-            TextBundle::from_section(credits_right_str, text_style.clone())
-                .with_text_justify(JustifyText::Left)
-                .with_style(Style {
-                    align_self: AlignSelf::Center,
-                    margin: UiRect {
-                        left: Val::Auto,
-                        right: Val::Auto,
-                        top: Val::Auto,
-                        bottom: Val::Auto,
-                    },
-                    padding: UiRect {
-                        left: Val::Px(400.0),
                         right: Val::Auto,
                         top: Val::Auto,
                         bottom: Val::Auto,
@@ -79,13 +43,13 @@ pub fn enter(mut commands: Commands, mut fonts: ResMut<Assets<Font>>) {
         )
         .insert(CreditsText)
         .id();
-    commands.insert_resource(MenuData {
-        credits_left,
-        credits_right,
-    });
+    // add a rectangular border to the credits text
+    commands
+        .entity(credits)
+        .insert(Outline::new(Val::Px(2.), Val::Px(10.0), Color::WHITE));
+    commands.insert_resource(MenuData { credits });
 }
 
 pub fn cleanup(mut commands: Commands, menu_data: Res<MenuData>) {
-    commands.entity(menu_data.credits_left).despawn_recursive();
-    commands.entity(menu_data.credits_right).despawn_recursive();
+    commands.entity(menu_data.credits).despawn_recursive();
 }
